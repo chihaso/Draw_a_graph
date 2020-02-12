6062a2cd7b2ca7fc8f2dafec6fcb11e4d9a390d7
ちはそ <cycyccq@gmail.com>
2020-02-12 22:23:25 +0900

    コード整形、スコープの限定 ・JSの変数はキャメルケースで統一 ・HTML、CSSのクラス名等はスネークケースで統一 ・JSの各変数のスコープを狭めて出来るだけconstで宣言

--- graph.js
+++ graph.js
@@ -1,67 +1,79 @@
 paper.install(window);
-paper.setup(document.getElementById("graph_canvas"));
+paper.setup(document.getElementById("graph-canvas"));
 
-let a, b, c;
-let x, y;
-
-function func(selected_func, x) {
-  switch (selected_func) {
-    case "quadratic_equation":
+function func(selectedFunc, x) {
+  let a, b, c, d;
+  switch (selectedFunc) {
+    case "quadratic-equation":
       a = 0.1;
       b = 1;
       c = 0;
       return a * x * x + b * x + c;
     case "sin":
-      a = 1;
-      b = 1;
+      a = 100;
+      b = 0.1;
       c = 0;
-      return a * 100 * Math.sin(b * 0.1 * x + c);
+      d = 0;
+      return a * Math.sin(b * x + c) + d;
     case "cos":
-      a = 1;
-      b = 1;
+      a = 100;
+      b = 0.1;
       c = 0;
-      return a * 100 * Math.cos(b * 0.1 * x + c);
+      d = 0;
+      return a * Math.cos(b * x + c) + d;
     case "tan":
-      a = 1;
-      b = 0.1;
+      a = 100;
+      b = 0.01;
       c = 0;
-      return a * 100 * Math.tan(b * 0.1 * x + c);
+      d = 0;
+      return a * Math.tan(b * x + c) + d;
     case "arcsin":
-      a = 1;
-      b = 1;
-      c = 0;
-      return a * 100 * Math.asin(b * 0.1 * x + c);
-    case "arccos":
-      a = 1;
-      b = 1;
-      c = 0;
-      return a * 100 * Math.acos(b * 0.1 * x + c);
-    case "arctan":
-      a = 1;
+      a = 100;
       b = 0.1;
       c = 0;
-      return a * 100 * Math.atan(b * 0.1 * x + c);
+      d = 0;
+      return a * Math.asin(b * x + c) + d;
+    case "arccos":
+      a = 100;
+      b = 0.1;
+      c = 0;
+      d = 0;
+      return a * Math.acos(b * x + c) + d;
+    case "arctan":
+      a = 100;
+      b = 0.1;
+      c = 0;
+      d = 0;
+      return a * Math.atan(b * x + c) + d;
   }
 }
 
-let current_func = document.getElementById("displayed_func");
+let currentFunc = document.getElementById("displayed-func");
 
-document.getElementById("button").onclick = function() {
-  const selected_func = document.functionForm._select.value;
-  current_func.textContent = selected_func;
+const xAxis = new Path();
+xAxis.add(new Point(0, 251), new Point(502, 251));
+xAxis.strokeColor = "black";
+const yAxis = new Path();
+yAxis.add(new Point(251, 0), new Point(251, 502));
+yAxis.strokeColor = "black";
+
+document.getElementById("button").onclick = () => {
+  const selectedFunc = document.functionForm._select.value;
   const path = new Path();
-  let dot;
-  let previous_point;
-  let current_point;
-  for (x = -250; x < 251; ++x) {
-    y = func(selected_func, x);
-    current_point = new Point(x + 250, 250 - y);
-    dot = Shape.Circle(x + 250, 250 - y, 2);
+  let previousPoint;
+
+  currentFunc.textContent = selectedFunc;
+
+  for (let x = -250; x < 251; x += 1) {
+    const y = func(selectedFunc, x);
+    const currentPoint = new Point(x + 250, 250 - y);
+    const dot = Shape.Circle(x + 250, 250 - y, 2);
     dot.fillColor = "black";
     path.strokeColor = "black";
-    if (!!previous_point) {
-      path.add(previous_point, current_point);
+
+    if (!!previousPoint) {
+      path.add(previousPoint, currentPoint);
     }
-    previous_point = current_point;
+    previousPoint = currentPoint;
   }
 };
