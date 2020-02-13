1f521c6bea7cd7da4eb18373b0715b8d66344ed9
ちはそ <cycyccq@gmail.com>
2020-02-13 14:07:25 +0900

    「表示中の関数」に式を表示する

--- graph.js
+++ graph.js
@@ -1,13 +1,32 @@
 paper.install(window);
 paper.setup(document.getElementById("graph-canvas"));
 
+function makeFormula(selectedFunc) {
+  switch (selectedFunc) {
+    case "quadratic-equation":
+      return "<span>y = a x(x + b) + c</span>";
+    case "sin":
+      return "<span>y = a sin (bx + c) + d</span>";
+    case "cos":
+      return "<span>y = a cos (bx + c) + d</span>";
+    case "tan":
+      return "<span>y = a tan (bx + c) + d</span>";
+    case "arcsin":
+      return "<span>y = a arcsin (bx + c) + d</span>";
+    case "arccos":
+      return "<span>y = a arccos (bx + c) + d</span>";
+    case "arctan":
+      return "<span>y = a arctan (bx + c) + d</span>";
+  }
+}
+
 function func(selectedFunc, x, a, b, c, d) {
   switch (selectedFunc) {
     case "quadratic-equation":
       a = a || 0.1;
-      b = b || 1;
+      b = b || 0.1;
       c = c || 0;
-      return a * x * x + b * x + c;
+      return x * (a * x + b) + c;
     case "sin":
       a = a || 100;
       b = b || 0.1;
@@ -46,42 +65,46 @@ function func(selectedFunc, x, a, b, c, d) {
       return a * Math.atan(b * x + c) + d;
   }
 }
-
 let currentFunc = document.getElementById("displayed-func");
 
-const xAxis = new Path();
-xAxis.add(new Point(0, 251), new Point(502, 251));
-xAxis.strokeColor = "black";
-for (let i = 51; i < 452; i += 50) {
-  const scale = new Path();
-  scale.add(new Point(i, 256), new Point(i, 246));
-  scale.strokeColor = "black";
-  const text = new PointText(i, 265);
-  text.justification = "center";
-  text.fillColor = "black";
-  text.fontSize = 10;
-  text.content = i - 251;
+function drawAxis() {
+  const xAxis = new Path();
+  const yAxis = new Path();
+
+  xAxis.add(new Point(0, 251), new Point(502, 251));
+  xAxis.strokeColor = "black";
+  for (let i = 51; i < 452; i += 50) {
+    const scale = new Path();
+    const text = new PointText(i, 265);
+    scale.add(new Point(i, 256), new Point(i, 246));
+    scale.strokeColor = "black";
+    text.justification = "center";
+    text.fillColor = "black";
+    text.fontSize = 10;
+    text.content = i - 251;
+  }
+  yAxis.add(new Point(251, 0), new Point(251, 502));
+  yAxis.strokeColor = "black";
+  for (let i = 51; i < 452; i += 50) {
+    const scale = new Path();
+    const text = new PointText(265, i + 5);
+    scale.add(new Point(256, i), new Point(246, i));
+    scale.strokeColor = "black";
+    text.justification = "left";
+    text.fillColor = "black";
+    text.fontSize = 10;
+    text.content = 251 - i;
+  }
 }
 
-const yAxis = new Path();
-yAxis.add(new Point(251, 0), new Point(251, 502));
-yAxis.strokeColor = "black";
-for (let i = 51; i < 452; i += 50) {
-  const scale = new Path();
-  scale.add(new Point(256, i), new Point(246, i));
-  scale.strokeColor = "black";
-  const text = new PointText(265, i + 5);
-  text.justification = "left";
-  text.fillColor = "black";
-  text.fontSize = 10;
-  text.content = 251 - i;
-}
+drawAxis();
 
-document.getElementById("button").onclick = () => {
+
+document.getElementById("drawButton").onclick = () => {
   const selectedFunc = document.functionForm._select.value;
   const path = new Path();
   let previousPoint;
-  currentFunc.textContent = selectedFunc;
+  currentFunc.innerHTML = makeFormula(selectedFunc);
   let a = document.setCoefficients.aValue.value;
   let b = document.setCoefficients.bValue.value;
   let c = document.setCoefficients.cValue.value;
